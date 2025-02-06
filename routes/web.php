b5fd20e50f694ad487ce0755703a44f6bed877a2
Stephen Abbott <sjabbott78@gmail.com>
2025-02-05 19:40:23 -0500

    Add route name for AdminerNeo index route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use SabbottLabs\AdminerNeo\Http\Controllers\AdminerNeoController;
 Route::match(['get', 'post'], '/', [AdminerNeoController::class, 'index'])
     ->middleware(config('adminerneo.middleware'))->withoutMiddleware([
         \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-    ]);
+    ])->name('adminerneo');
